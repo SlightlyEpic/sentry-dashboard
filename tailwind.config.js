a7e876984bc77d05259f3050d3e288e54553815f
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-01-15 15:03:35 +0530

    Skeleton

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/*.{js,ts,jsx,tsx,css}",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blurple': '#5865f2',
+        'blurple-l': '#707ae6',
+        'bgdark': '#17181a',
+        'bgdark-l': '#242629',
+      }
+    },
+  },
+  plugins: [],
+}
+
