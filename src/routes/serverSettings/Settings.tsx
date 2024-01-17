dbd3fa674596c31f6a350927bef3a1e347b19f8f
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-01-17 12:34:20 +0530

    Working loader

--- src/routes/serverSettings/Settings.tsx
+++ src/routes/serverSettings/Settings.tsx
@@ -1,12 +1,17 @@
 import { Navmenu } from '@/components/Navmenu';
-import { RouteObject, Outlet, useLoaderData } from 'react-router-dom';
+import { RouteObject, Outlet, useLoaderData, LoaderFunction } from 'react-router-dom';
 import { ExtractRouteParams } from '@/util/typeMagic';
 
 export const routerData: RouteObject = {
     path: 'settings/:serverId/',
     element: <Settings />,
+    loader: loader as unknown as LoaderFunction,
     children: [
-        { index: true, element: <Index /> }
+        {
+            index: true,
+            element: <Index />,
+            loader: loader as unknown as LoaderFunction
+        }
     ]
 }
 
@@ -14,18 +19,16 @@ type RouteParams = ExtractRouteParams<'settings/:serverId/'>;
 type LoaderData = Awaited<ReturnType<typeof loader>>;
 
 export async function loader({ params }: { params: RouteParams }) {
-    console.log('params:', params);
     return {
         serverId: params.serverId
     };
 }
 
 function Index() {
-    // const loaderData = useLoaderData() as LoaderData;
-    // console.log('loader data:', loaderData);
+    const loaderData = useLoaderData() as LoaderData;
+    console.log('loader data:', loaderData);
 
-    // return <div className='text-white'>Index: {loaderData.serverId}</div>;
-    return <div className='text-white'>Index</div>;
+    return <div className='text-white'>Index: {loaderData.serverId}</div>;
 }
 
 export default function Settings() {
@@ -35,7 +38,7 @@ export default function Settings() {
     return (
         <div className="flex justify-start min-h-screen bg-bgdark">
             {/* <Navmenu serverId={loaderData.serverId} /> */}
-            <Navmenu serverId='1234' />
+            <Navmenu serverId={loaderData.serverId} />
             <div className='w-full h-full relative'>
                 <Outlet />
             </div>
