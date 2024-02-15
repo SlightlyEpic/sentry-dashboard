029f6add5d14d775a89c475e8665c9be961131ad
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-02-15 15:47:21 +0530

    Remove mui

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 import { routerData as appRouterData } from '@/routes/App';
 import { routerData as serverSelectorRouterData } from '@/routes/settings/ServerSelector';
