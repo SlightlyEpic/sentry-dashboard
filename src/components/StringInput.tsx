adb9f78e43d2445988de79edeb37fc56cf353a4e
duskfirst <122384892+duskfirst@users.noreply.github.com>
2024-02-19 16:30:57 +0530

    InputString Component added

---
+++ src/components/StringInput.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+interface StringInputProps {
+    text: string;
+    onSave: () => void;
+    onEdit: () => void;
+    isEdit: boolean;
+}
+
+const StringInput = ({ text, onSave, onEdit, isEdit }: StringInputProps) => {
+
+    return (
+        <>
+            <div className='flex justify-between border border-bggrey-ll p-2 rounded-md bg-bgdark m-2'>
+                {!isEdit && <>
+                    <div className='place-self-center'>
+                        {text}
+                    </div>
+
+
+                    <button type="button" className='text-white self-start mt-auto bg-blurple hover:bg-bgdark
+                        hover:ring-2 hover:ring-blurple-l rounded-md text-md font-bold px-4 py-1'
+                        onClick={onEdit}
+                    >
+                        Edit
+                    </button>
+                </>
+                }
+
+                {isEdit &&
+                    <>
+                        <input type="text" className='bg-bggrey-ll rounded-md
+                                focus:outline-none p-1 focus:outline-1 focus:outline-blurple-ll'
+                            id={text}
+                            defaultValue={text}        
+                        />
+                        <button type="button" className='text-white self-start mt-auto bg-blurple hover:bg-bgdark
+                        hover:ring-2 hover:ring-blurple-l rounded-md text-md font-bold px-4 py-1'
+                            onClick={onSave}
+                        >
+                            Save
+                        </button>
+                    </>
+                }
+            </div>
+        </>
+    )
+}
+
+export default StringInput
--- src/routes/serverSettings/general/General.tsx
+++ src/routes/serverSettings/general/General.tsx
@@ -1,3 +1,5 @@
+import StringInput from '@/components/StringInput';
+import { useState } from 'react';
 import { RouteObject } from 'react-router-dom';
 
 export const routerData: RouteObject = {
@@ -5,9 +7,78 @@ export const routerData: RouteObject = {
     element: <GeneralSettings />,
 }
 
+
+
 export default function GeneralSettings() {
+    const [isEdit, setEdit] = useState(false);
+
+    const onEdit = () => {
+        setEdit(!isEdit);
+    }
+
+    const onSave = () => {
+        setEdit(!isEdit);
+    }
+    const prefix = 'p';
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-6 justify-start h-full w-full [&>*]:place-self-center">
+        <div className="justify-start flex my-4 flex-col h-fit w-full bg-bgdark-l rounded-md border-2  border-bggrey-l text-white">
+
+            <div className='self-center h-fit w-fit p-2 m-2 text-4xl'>General Settings</div>
+
+            <ul className='p-2 m-2'>
+
+                <li className='items-center p-2 m-2'>
+                    <div className='text-xl p-2 m-2'>Prefix</div>
+                    
+                    <StringInput onSave={onSave} onEdit={onEdit} isEdit={isEdit} text={prefix} />
+                </li>
+
+                <li className='items-center p-2 m-2'>
+                    <div className='text-xl p-2 m-2'>Moderation Stats</div>
+                    
+                    <div className='flex justify-between border border-bggrey-ll p-2 rounded-md bg-bgdark m-2'>
+                        <div className='place-self-center'>
+                            clear 
+                        </div>
+                        
+                        
+                        <button type="button" className='text-white self-start mt-auto bg-blurple hover:bg-bgdark
+                                            hover:ring-2 hover:ring-blurple-l rounded-md text-md font-bold px-4 py-1'
+                        >
+                            Edit
+                        </button>
+                    </div>
+
+
+                </li>
+                <li className='items-center p-2 m-2'>
+                    
+                    <div className='text-xl p-2 m-2'>Compact Response</div>
+                    
+                    <div className='flex justify-between border border-bggrey-ll p-2 rounded-md bg-bgdark m-2'>
+                        <div className='mr-2 place-self-center'>
+                            abcdefg
+                        </div>
+                        
+                        
+                        <button type="button" className='text-white self-start mt-auto bg-blurple hover:bg-bgdark
+                                            hover:ring-2 hover:ring-blurple-l rounded-md text-md font-bold px-4 py-1'
+                        >
+                            Edit
+                        </button>
+                    </div>
+                </li>
+                <li className='items-center justify-start flex p-2 m-2 text-xl'>
+                    <div className='p-2 m-2'>
+                        Premium
+                        
+                    </div>
+                    <div>
+                        YES
+                    </div>
+                </li>
+            </ul>
+
         </div>
     )
 }
