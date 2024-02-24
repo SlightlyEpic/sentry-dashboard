943678cc9fd73f4a71470cb0384a63a50f808c0e
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-02-25 00:19:35 +0530

    Added redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import './index.css';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { routerData as appRouterData } from '@/routes/App';
 import { routerData as loginRouterData } from '@/routes/Login';
@@ -22,7 +23,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <RouterProvider router={router} />
+            <PersistGate persistor={persistor}>
+                <RouterProvider router={router} />
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import guildReducer from './guildSlice';
+import { persistStore, persistReducer } from 'redux-persist'
+import storageSession from 'redux-persist/lib/storage/session'
+
+const persistConfig = {
+    key: 'root',
+    storage: storageSession,
+};
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    guild: guildReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        guild: guildReducer
-    }
+    reducer: persistedReducer
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
